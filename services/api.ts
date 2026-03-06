d7a4f5418416d3fe822894974c9e771969de3156
vivek kumar <viveksnh933@gmail.com>
2026-03-06 16:46:34 +0530

    updated

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  baseURL: "https://hrms-backend-cqqc.onrender.com",
 });
 
 export default API;
